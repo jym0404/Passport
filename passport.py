99a02958b75457533f7cf1bfca7a4677663074bf
jym0404 <49983304+jym0404@users.noreply.github.com>
2019-07-24 13:25:31 +0900

    Add files via upload

---
+++ passport.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+class Passport:
+    def __init__(self, name, nat):
+        now = datetime.now()
+        self.doi = str(now.year)+"/"+str(now.month)+"/"+str(now.day)
+        self.name = name
+        self.nara = nat
+        self.wg = []
+    def add_nation(self, nat):
+        self.wg = self.wg + [nat]
+    def print_passport(self):
+        print("---------------------------")
+        print(" ")
+        print("name:",self.name)
+        print("nationality:",self.nara)
+        print("date of issue:",self.doi)
+        print(" ")
+        print("---------------------------")
+        for i in range(len(self.wg)):
+            print(" ")
+            print(self.wg[i])
+            print(" ")
+            print("---------------------------")
+    def edit_owner_info(self, name, nat):
+        self.name = name
+        self.nara = nat
+        self.wg = []
+#Ex:
+#person1 = Passport("daniel", "korea")
+#person1.add_nation("china")
+#person1.add_nation("canada")
+#person1.print_passport()
